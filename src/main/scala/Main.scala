a3f42e598ad6e245d6319d454aaef5d05b506cb8
Azat Nobatov <azatprog@gmail.com>
2018-04-06 22:16:46 +0300

    load model, stream model, console the trained tweets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,55 @@
+import MLExample.{df, toArray}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming._
 import org.apache.spark.SparkConf
 import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
 import org.apache.spark.ml.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.{Row, SparkSession, functions}
+import org.apache.spark.streaming.dstream.DStream
 
 object Main extends App {
-  val sc = org.apache.spark.sql.SparkSession.builder
-    .master("local")
-    .appName("Spark CSV Reader")
-    .getOrCreate
+  val conf = new SparkConf().setMaster("local[2]").setAppName("Spark CSV Reader")
+  val ssc = new StreamingContext(conf, Seconds(15))
+
+  //  val sc = org.apache.spark.sql.SparkSession.builder
+  //    .master("local")
+  //    .appName("Spark CSV Reader")
+  //    .getOrCreate
 
   val w2vModel = Word2VecModel.load("Word2Vec")
   val lrModel = LogisticRegressionModel.load("LogisticRegression")
 
-//  val ssc = new StreamingContext(sc, Seconds(15))
-//  val stream = TwitterUtils.createStream(ssc, None)
-//  case class Tweet(createdAt:Long, text:String)
-//  val twits = stream.window(Seconds(60)).map(m=>
-//    Tweet(m.getCreatedAt().getTime()/1000, m.toString)
-//  )
-//
-//  twits.foreachRDD(rdd => rdd.collect().foreach(println))
-//  ssc.start()
-//  ssc.awaitTermination()
+  //  val ssc = new StreamingContext(sc, Seconds(15))
+  case class Tweet(createdAt: Long, text: String)
+
+  val stream = TwitterUtils.createStream(ssc, None)
+  val twits = stream.window(Seconds(60)).map(m =>
+    Tweet(m.getCreatedAt().getTime() / 1000, m.getText)
+    //    Tweet(1, "love good happy")
+  )
+
+
+  twits.foreachRDD { rdd =>
+    // Get the singleton instance of SparkSession
+    val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+    import spark.implicits._
+
+    val toArray = functions.udf[Array[String], String](_.split(" "))
+
+    // Convert RDD[String] to DataFrame
+    val wordsDataFrame = rdd.toDF("createdAt", "text")
+    val trans = w2vModel.transform(wordsDataFrame
+      .withColumn("array", toArray(wordsDataFrame("text"))))
+
+    lrModel.transform(trans)
+      .select("text", "prediction")
+      .collect()
+      .foreach { case Row(text: String, prediction: Double) =>
+        println("blablablah", text, prediction)
+      }
+
+  }
+    ssc.start()
+    ssc.awaitTermination()
 }
